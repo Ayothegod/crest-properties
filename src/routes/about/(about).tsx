281f65a37cbac64064ef80a67252a96852892a73
Ayothegod <heyayomideadebisi@gmail.com>
2024-07-22 20:38:45 +0100

    add routes

---
+++ src/routes/about/(about).tsx
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+      <div class="grid place-items-center min-h-screen">
+        <div class="h-[160px] aspect aspect-[2] rounded-[16px] shadow-[0_0_0_4px_hsl(0_0%_0%_/_15%)]">
+          About
+        </div>
+      </div>
+    );
+  }
+  
