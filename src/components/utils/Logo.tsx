6259c0b20a07d64a6c772609765ab4ae20c54cae
Ayothegod <heyayomideadebisi@gmail.com>
2024-07-26 15:50:49 +0100

    remove solidstart

--- src/components/utils/Logo.tsx
+++
@@ -1,3 +0,0 @@
-export default function Logo({ text, url, image }: any) {
-  return <div class="font-bold text-3xl">CrestProperties</div>;
-}
--- src/routes/(resources)/faq/(faq).tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Faq() {
-  return (
-    <>
-    <div>Hello faq, haha</div>
-    </>
-  )
-}
