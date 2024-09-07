e0998c0cd20d55135eb9df1d0ddaafd1dfeeb521
Ayothegod <heyayomideadebisi@gmail.com>
2024-09-07 18:24:17 +0100

    init astro

---
+++ astro.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import node from '@astrojs/node';
+
+import tailwind from '@astrojs/tailwind';
+
+import mdx from '@astrojs/mdx';
+
+import react from '@astrojs/react';
+
+import db from '@astrojs/db';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone'
+  }),
+
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), mdx(), react(), db()]
+});
