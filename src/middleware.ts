82b5582ea680fe8c95c57f15d52d5ebc6cec5eaf
Ayothegod <heyayomideadebisi@gmail.com>
2024-09-13 08:53:27 +0100

    add middleware to protect login and register routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,19 +3,19 @@ import { lucia } from "./auth";
 import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware(async (context, next) => {
-// console.log("Middleware");
+  // console.log("Middleware");
+  
+  const sessionId = context.cookies.get(lucia.sessionCookieName)?.value ?? null;
+  if (!sessionId) {
+    context.locals.user = null;
+    context.locals.session = null;
+    return next();
+  }
 
-const sessionId = context.cookies.get(lucia.sessionCookieName)?.value ?? null;
-if (!sessionId) {
-	context.locals.user = null;
-	context.locals.session = null;
-	return next();
-}
-
-const { session, user } = await lucia.validateSession(sessionId);
-
-if (session && session.fresh) {
-	const sessionCookie = lucia.createSessionCookie(session.id);
+  const { session, user } = await lucia.validateSession(sessionId);
+  
+  if (session && session.fresh) {
+    const sessionCookie = lucia.createSessionCookie(session.id);
     context.cookies.set(
       sessionCookie.name,
       sessionCookie.value,
@@ -25,13 +25,18 @@ if (session && session.fresh) {
   if (!session) {
     const sessionCookie = lucia.createBlankSessionCookie();
     context.cookies.set(
-			sessionCookie.name,
+      sessionCookie.name,
       sessionCookie.value,
       sessionCookie.attributes,
     );
   }
   context.locals.session = session;
   context.locals.user = user;
+
+  const pathname = context.url.pathname;
+  if (user && (pathname === "/login" || pathname === "/register")) {
+    return context.redirect("/");
+  }
 
   return next();
 });
